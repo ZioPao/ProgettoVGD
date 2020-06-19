230a239ae0c671e91cb81d3da04430a98450b69e
ZioPao <picchetti1998@gmail.com>
2020-06-19 18:18:43 +0200

    L'acqua ora non è più ferma ma si muove leggerissimamente

--- Assets/MovingWater.cs
+++ Assets/MovingWater.cs
@@ -4,18 +4,37 @@ using UnityEngine;
 
 public class MovingWater : MonoBehaviour
 {
-    Terrain water;
+
+    [SerializeField] public float countdownMovingWater = 1.0f;
+
+    private float countdownMovingWaterCurrent;
     // Start is called before the first frame update
+    bool isMovingUp = true;
+
     void Start()
     {
-        water = GetComponent<Terrain>();
-
+        countdownMovingWaterCurrent = countdownMovingWater;
     }
 
     private void FixedUpdate()
     {
+        if (isMovingUp)
+        {
+            countdownMovingWaterCurrent -= Time.deltaTime;
+            transform.position += new Vector3(0, 0.001f, 0);
 
-        var layerTmp = water.terrainData.terrainLayers.GetValue(0);
-        print("we've got it");
+            if (countdownMovingWaterCurrent <= 0)
+                isMovingUp = false;
+        }
+        else
+        {
+            countdownMovingWaterCurrent += Time.deltaTime;
+            transform.position -= new Vector3(0, 0.001f, 0);
+
+            if (countdownMovingWaterCurrent >= countdownMovingWater)
+                isMovingUp = true;
+        }
+
+
     }
 }
