dc3f6a60d1de22081a252b68dc82d53dd1ce31c4
ZioPao <picchetti1998@gmail.com>
2020-06-19 18:31:28 +0200

    Anche la texture dell'acqua ora si muove

--- Assets/MovingWater.cs
+++ Assets/MovingWater.cs
@@ -8,12 +8,14 @@ public class MovingWater : MonoBehaviour
     [SerializeField] public float countdownMovingWater = 1.0f;
 
     private float countdownMovingWaterCurrent;
+    private Material waterMaterial;
     // Start is called before the first frame update
     bool isMovingUp = true;
 
     void Start()
     {
         countdownMovingWaterCurrent = countdownMovingWater;
+        waterMaterial = GetComponentInChildren<Renderer>().material;
     }
 
     private void FixedUpdate()
@@ -22,7 +24,7 @@ public class MovingWater : MonoBehaviour
         {
             countdownMovingWaterCurrent -= Time.deltaTime;
             transform.position += new Vector3(0, 0.001f, 0);
-
+            waterMaterial.mainTextureScale -= new Vector2(0.002f, 0);
             if (countdownMovingWaterCurrent <= 0)
                 isMovingUp = false;
         }
@@ -30,6 +32,7 @@ public class MovingWater : MonoBehaviour
         {
             countdownMovingWaterCurrent += Time.deltaTime;
             transform.position -= new Vector3(0, 0.001f, 0);
+            waterMaterial.mainTextureScale += new Vector2(0.002f, 0);
 
             if (countdownMovingWaterCurrent >= countdownMovingWater)
                 isMovingUp = true;
