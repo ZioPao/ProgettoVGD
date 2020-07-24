febc67fc1fd96718d0c6804ee5a50f142a2b08aa
ZioPao <picchetti1998@gmail.com>
2020-07-24 02:28:07 +0200

    PERCHE NON COOMMITTI TUTTO AAAAAAAAAAAAAAAAAA

---
+++ Assets/Scripts/Utility/LeverStatus.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Utility
+{
+    public class LeverStatus
+    {
+        private bool isMoving;
+        private string doorName;
+        private Transform movingPiece;
+
+
+        public void SetIsMoving(bool value)
+        {
+            isMoving = value;
+        }
+
+        public void SetDoorName(string name)
+        {
+            doorName = name;
+        }
+
+        public void SetMovingPiece(Transform t)
+        {
+            movingPiece = t;
+        }
+        public bool GetIsMoving()
+        {
+            return isMoving;
+        }
+
+        public string GetDoorName()
+        {
+            return doorName;
+        }
+
+        public Transform GetMovingPiece()
+        {
+            return movingPiece;
+        }
+    }
+}
