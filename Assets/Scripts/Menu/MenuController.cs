888c11d34167ed492a4ff8652e863b80bf9a6740
ZioPao <picchetti1998@gmail.com>
2020-07-28 13:16:09 +0200

    iniziando ad appiccicare il tutto

--- Assets/Scripts/Menu/MenuController.cs
+++ Assets/Scripts/Menu/MenuController.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,16 +9,37 @@ namespace Menu
 {
     public class MenuController : MonoBehaviour
     {
-        
+        private Material skybox;
+
         public void NewGame()
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            SceneManager.LoadScene("BaseGame");
+
+            SceneManager.activeSceneChanged += OnSceneWasLoaded;
+            
+
         }
 
         public void QuitGame()
         {
             UnityEditor.EditorApplication.isPlaying = false;
             Application.Quit();
+        }
+
+        private static void OnSceneWasLoaded (Scene from, Scene to) {
+            var playerPrefab = Resources.Load<GameObject>("Prefabs/Player");
+            var level = Resources.Load<GameObject>("Prefabs/Levels/Level1");
+
+            PrefabUtility.InstantiatePrefab(level);
+            var playerInstance = PrefabUtility.InstantiatePrefab(playerPrefab) as GameObject;        //Where?
+            RenderSettings.skybox = Resources.Load<Material>("Prefabs/Levels/Level1/Skybox");;
+            //debug
+            var spawnPoint = new Vector3(1237, 115, 1041);
+            //spawnPoint = new Vector3(145, 67, 40); //level 3
+            
+            
+            //spawnPoint = new Vector3()
+            playerInstance.transform.position = spawnPoint;
         }
 
     }
---
+++ Assets/Scripts/Utility/HealthScript.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Player;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        this.transform.Rotate(0, 2, 0, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (Values.GetHealth() < 100)
+        {
+            Values.AddHealth(25);
+            Destroy(gameObject);
+        }
+
+        //when players touch it automatically add tot health to him
+    }
+}    
