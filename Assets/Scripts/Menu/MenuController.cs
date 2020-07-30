2674866701af739c2ba378fca8a222cb8773f037
ZioPao <picchetti1998@gmail.com>
2020-07-30 18:46:16 +0200

    tutto su singola scena se no il baking muore

--- Assets/Scripts/Menu/MenuController.cs
+++ Assets/Scripts/Menu/MenuController.cs
@@ -15,51 +15,46 @@ namespace Menu
 
         public void NewGame()
         {
-            SceneManager.LoadScene("BaseGame");
-
-            SceneManager.activeSceneChanged += NewGameLoadingScene;
-            
-
-        }
-
-        public void LoadGame()
-        {
-            SceneManager.LoadScene("BaseGame");
-            SceneManager.activeSceneChanged += LoadGameLoadingScene;
-
-        }
-        public void QuitGame()
-        {
-            UnityEditor.EditorApplication.isPlaying = false;
-            Application.Quit();
-        }
-
-        private static void NewGameLoadingScene (Scene from, Scene to) {
+            //SceneManager.LoadScene("BaseGame");
             var playerPrefab = Resources.Load<GameObject>("Prefabs/Player");
             var level = Resources.Load<GameObject>("Prefabs/Levels/Level1");
 
             PrefabUtility.InstantiatePrefab(level);
-            var playerInstance = PrefabUtility.InstantiatePrefab(playerPrefab) as GameObject;        //Where?
-            RenderSettings.skybox = Resources.Load<Material>("Prefabs/Levels/Level1/Skybox");;
+            var playerInstance = PrefabUtility.InstantiatePrefab(playerPrefab) as GameObject; //Where?
+            RenderSettings.skybox = Resources.Load<Material>("Prefabs/Levels/Level1/Skybox");
+            ;
+
+
             //debug
             var spawnPoint = new Vector3(1237, 115, 1041);
             //spawnPoint = new Vector3(145, 67, 40); //level 3
-            
-            
+
+
             //spawnPoint = new Vector3()
             playerInstance.transform.position = spawnPoint;
+
+            //Una volta finito tutto, distrugge il menù
+
+            Destroy(GameObject.Find("MainMenu"));
+            //SceneManager.activeSceneChanged += NewGameLoadingScene;
         }
 
-        private static void LoadGameLoadingScene(Scene from, Scene to)
+        public void LoadGame()
         {
             var playerPrefab = Resources.Load<GameObject>("Prefabs/Player");
-            var player = PrefabUtility.InstantiatePrefab(playerPrefab) as GameObject;        //Where?
-   
+            var player = PrefabUtility.InstantiatePrefab(playerPrefab) as GameObject;
+
             SaveSystem tmp = player.AddComponent<SaveSystem>();
 
             tmp.Load();
-            Destroy(tmp);      
+            Destroy(tmp);
+            Destroy(GameObject.Find("MainMenu"));
+        }
 
+        public void QuitGame()
+        {
+            EditorApplication.isPlaying = false;
+            Application.Quit();
         }
     }
 }
