b9a7e83338b0b139a966567fd764483e01d81e37
ZioPao <picchetti1998@gmail.com>
2020-07-31 15:48:30 +0200

    boss 2 sprites

--- Assets/Scripts/Boss/BossTwo.cs
+++ Assets/Scripts/Boss/BossTwo.cs
@@ -34,7 +34,7 @@ namespace Boss
     /*  Inizialmente identico a Nemico 1, quando subisce una certa quantità di danni cambia fase diventando molto più
         aggressivo (Attacca sparando un maggior numero di proiettili)
     */
-        private void Start()
+        private void Awake()
         {
 
             boss = GetComponent<EnemyBase>();
@@ -43,6 +43,8 @@ namespace Boss
             
             //Set boss health
             boss.GetStatus().SetHealth(bossHealth);
+
+            Values.SetCurrentBoss(gameObject);
 
             
             
