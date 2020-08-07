06ca8a6961fc35b09a309952fdfb28e2c54a8c18
ZioPao <picchetti1998@gmail.com>
2020-08-07 13:50:54 +0200

    aggiunte player prefs per settings

--- Assets/Scripts/Menu/SliderManager.cs
+++ Assets/Scripts/Menu/SliderManager.cs
@@ -6,41 +6,68 @@ namespace Menu
 {
     public class SliderManager : MonoBehaviour
     {
-
         [SerializeField] private Slider effectSlider, musicSlider, sensibilitySlider;
+
+        private const string EffectKey = "effectSlider";
+        private const string MusicKey = "musicSlider";
+        private const string SensibilityKey = "sensibilitySlider";
+
 
         void Awake()
         {
-            effectSlider.value = 0.5f;
-            musicSlider.value = 0.5f;
-            sensibilitySlider.value = 0.5f;
+
+            float effectSliderValue, musicSliderValue, sensibilityValue;
+
+            ManagePrefs(EffectKey, out effectSliderValue);
+            ManagePrefs(MusicKey, out musicSliderValue);
+            ManagePrefs(SensibilityKey, out sensibilityValue);
+
+            effectSlider.value = effectSliderValue;
+            musicSlider.value = musicSliderValue;
+            sensibilitySlider.value = sensibilityValue;
 
             effectSlider.onValueChanged.AddListener(delegate { EffectSliderCheck(); });
             musicSlider.onValueChanged.AddListener(delegate { MusicSliderCheck(); });
             sensibilitySlider.onValueChanged.AddListener(delegate { SensibilitySliderCheck(); });
-
         }
 
         private void EffectSliderCheck()
         {
             Audio.SoundManager.SetEffectVolumeModifier(effectSlider.value);
+            PlayerPrefs.SetFloat(EffectKey, effectSlider.value);
+            PlayerPrefs.Save();
+            
         }
 
         private void MusicSliderCheck()
         {
             Audio.SoundManager.SetTrackVolumeModifier(musicSlider.value);
+            PlayerPrefs.SetFloat(MusicKey, effectSlider.value);
+            PlayerPrefs.Save();
         }
 
         private void SensibilitySliderCheck()
         {
             Values.SetMouseSensibility(sensibilitySlider.value);
-            
+
             //evitiamo di mettere altri valori in Values, ormai è saturo
             if (Values.GetPlayerTransform())
             {
                 Values.GetPlayerTransform().GetComponentInChildren<CameraScript>().UpdateMouseSensibility();
-
             }
+            PlayerPrefs.SetFloat(SensibilityKey, sensibilitySlider.value);
+            PlayerPrefs.Save();
+        }
+
+        private void ManagePrefs(string key, out float value)
+        {
+            if (!PlayerPrefs.HasKey(key))
+            {
+                PlayerPrefs.SetFloat(key, 0.5f);
+            }
+
+            value = PlayerPrefs.GetFloat(key);
+
         }
     }
 }
